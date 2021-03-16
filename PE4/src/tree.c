694e527005353e49ed8211bcd989f18db225708c
Mikael Kvalvaer <mikaelkvalvaer@gmail.com>
2021-03-16 20:58:39 +0100

    Fix 4.2 and comment out 4.1
    4.1 does not seem to work as we wish, so we'll have to fix that. In the
    meantime, we get nice results if we just comment it out

--- PE4/src/tree.c
+++ PE4/src/tree.c
@@ -65,48 +65,78 @@ destroy_subtree ( node_t *discard )
 }
 
 //TODO: Implement simplify_tree.
+int is_container_type(node_t *node) {
+    return node->type == GLOBAL_LIST || 
+        (node->type >= STATEMENT_LIST && node->type <= DECLARATION_LIST);
+}
 
 void
 simplify_tree ( node_t **simplified, node_t *root )
 {
 
+
     for(int i = 0; i < root->n_children; i++){
-        
         simplify_tree(simplified, root->children[i]);
-
-        //4.1
-        if(root->children[i]->n_children == 1 && root->children[i]->data == NULL){
-        root->children[i] = root->children[i]->children[0];
-        }
-
-        //4.2 don't know if i'm doing what im supposed to here.
-        node_index_t child_type = root->children[i]->type;         
-        if(child_type == 1 || 2 < child_type < 10){//List have type value of 1 or between 3 to 9
-            node_t *temp = malloc(sizeof(root->children)+sizeof(root->children[i]->children)-sizeof(node_t));
-            root->n_children = root->children + root->children[i]->n_children - 1;
-            int t = 0;
-            for(int j = 0; j < root->n_children; j++){
-                if(j==i){
-                    for(; j < i + root->children[i]->n_children; j++){
-                        temp[j] = *root->children[i]->children[j-i];
-                    }
-                }
-                temp[j] = *root->children[t];
-                t++;
-            }
-            root->children = temp;
-        }
-
-        //4.3
-        //TODO: Compute the value of the subtrees representing arithmetic operations with constants and replace them with the value.
-        //step 1) all expresions that is on the form number operator number should them selvs be the resluting number. 
-        //Example: expresion is 1+2 then the expresion can be reduced to the number 3. The whole expresion node shoud 
-        //change its semantical data to 3 and type to NUMBER.
-        //step 2) all expresions that has an "=" relation to a number is themselvs a number.
-        //Example: expresion = 3 then the expresion can be reduced to the number 3. The whole relation node should 
-        //change its semantical data to 3 and type to NUMBER.
-
     }
+        
+
+    /* //4.1 */
+    /* for (int i = 0; i < root->n_children; i++) { */
+    /*     node_t* child = root->children[i]; */
+    /*     if(child->n_children == 1 && child->data == NULL){ */
+    /*         root->children[i] = child->children[0]; */
+    /*     } */
+    /* } */
+
+
+    // 4.2 -- flattening list structure
+    if (is_container_type(root)) {
+        for (int j = 0; j < root->n_children; j++) {
+            node_t *child = root->children[j];
+            if (child->type == root->type) {  // steal its children
+                switch (child->n_children) {
+                    case 0:
+                        fprintf(stderr, "Expected at least one child.");
+                        exit(1);
+                    case 1:
+                        root->children[j] = child->children[0];
+                        break;
+                    default:
+                        root->children[j] = child->children[0];
+                        root->children = realloc(
+                            root->children, 
+                            sizeof(node_t*) * (root->n_children + child->n_children - 1)
+                        );
+                        if (root->children == NULL) { fprintf(stderr, "Unable to realloc."); exit(1); }
+                        for (int j = 1; j < child->n_children; j++) {
+                            root->children[root->n_children++] = child->children[j];
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    // 4.2 print statement
+    if ( root->type == PRINT_STATEMENT) {
+        if (root->n_children != 1) {
+            fprintf(stderr, "Expected node of PRINT_STATEMENT type to have only one child.");
+            exit(1);
+        }
+        node_t *child = root->children[0];
+        root->children = child->children;
+        root->n_children = child->n_children;
+    }
+
+    //4.3
+    //TODO: Compute the value of the subtrees representing arithmetic operations with constants and replace them with the value.
+    //step 1) all expresions that is on the form number operator number should them selvs be the resluting number. 
+    //Example: expresion is 1+2 then the expresion can be reduced to the number 3. The whole expresion node shoud 
+    //change its semantical data to 3 and type to NUMBER.
+    //step 2) all expresions that has an "=" relation to a number is themselvs a number.
+    //Example: expresion = 3 then the expresion can be reduced to the number 3. The whole relation node should 
+    //change its semantical data to 3 and type to NUMBER.
+
 
 
     *simplified = root;
